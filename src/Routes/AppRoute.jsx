ae74e3f292cc0647734c0a2c39ff005c9af9c105
ibnu choldun <ibnucholdun023@gmail.com>
2022-11-22 16:30:04 +0700

    create sign in page

--- src/Routes/AppRoute.jsx
+++ src/Routes/AppRoute.jsx
@@ -3,13 +3,14 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 
 // Pages
-import { LandingPage, SignUp } from "./../Pages";
+import { LandingPage, SignUp, SignIn } from "./../Pages";
 
 const AppRouter = () => {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/sign-up" element={<SignUp />} />
+      <Route path="/sign-in" element={<SignIn />} />
     </Routes>
   );
 };
