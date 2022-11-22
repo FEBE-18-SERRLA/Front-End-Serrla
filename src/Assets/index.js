b6e6a7640ff88c38a01f56742695f2295924292b
ibnu choldun <ibnucholdun023@gmail.com>
2022-11-22 16:51:56 +0700

    create home page

--- src/Assets/index.js
+++ src/Assets/index.js
@@ -2,10 +2,14 @@ import Gambar1 from "./images/gambar1.png";
 import Gambar2 from "./images/gambar2.png";
 import Gambar3 from "./images/gambar3.png";
 import Gambar4 from "./images/gambar4.png";
+import Gambar5 from "./images/gambar5.png";
+import Gambar6 from "./images/gambar6.jpg";
+import Gambar7 from "./images/gambar7.jpg";
 import Google from "./images/google.png";
 import VectorOval from "./images/vector-oval.png";
 import Vector2 from "./images/vector2.png";
 import Vector3 from "./images/vector3.png";
+import Vector4 from "./images/vector4.png";
 import Logo from "./images/icon-serrla.png";
 import PlaceholderUser from "./images/placeholder-user.png";
 
@@ -14,10 +18,14 @@ export {
   Gambar2,
   Gambar3,
   Gambar4,
+  Gambar5,
+  Gambar6,
+  Gambar7,
   Google,
   VectorOval,
   Vector2,
   Vector3,
+  Vector4,
   Logo,
   PlaceholderUser,
 };
--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,3 +1,4 @@
 export { default as LandingPage } from "./01-LandingPage/LandingPage";
 export { default as SignUp } from "./02-SignUp/SignUp";
 export { default as SignIn } from "./03-SignIn/SignIn";
+export { default as Home } from "./04-HomePage/Home";
