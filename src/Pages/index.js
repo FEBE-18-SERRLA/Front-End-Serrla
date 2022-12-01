79edf42e525b0f17b4c12e9506476eadf5cf5a3c
Rafid Ammar <rafid.adinegoro@gmail.com>
2022-12-01 16:25:35 +0700

    create detail quiz

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -4,3 +4,4 @@ export { default as SignIn } from "./03-SignIn/SignIn";
 export { default as Home } from "./04-HomePage/Home";
 export { default as Modul } from "./05-ModulPage/Modul";
 export { default as DetailModul } from "./06-DetailModulPage/DetailModul";
+export { default as DetailQuiz } from "./DetailQuizPage/DetailQuizPage";
--- src/Routes/AppRoute.jsx
+++ src/Routes/AppRoute.jsx
@@ -4,33 +4,39 @@ import { Route, Routes } from "react-router-dom";
 
 // Pages
 import {
-  LandingPage,
-  SignUp,
-  SignIn,
-  Home,
-  Modul,
-  DetailModul,
+	LandingPage,
+	SignUp,
+	SignIn,
+	Home,
+	Modul,
+	DetailModul,
+	Quiz,
+	DetailQuiz,
 } from "./../Pages";
 
 const AppRouter = () => {
-  const isLogin = localStorage.getItem("user");
-  return (
-    <Routes>
-      {isLogin ? (
-        <Route path="/" element={<Home />} />
-      ) : (
-        <>
-          <Route path="/" element={<LandingPage />} />
-        </>
-      )}
-      <Route path="/sign-up" element={<SignUp />} />
-      <Route path="/sign-in" element={<SignIn />} />
-      <Route path="/modul">
-        <Route path="" element={<Modul />} />
-        <Route path="detail-modul/:id" element={<DetailModul />} />
-      </Route>
-    </Routes>
-  );
+	const isLogin = localStorage.getItem("user");
+	return (
+		<Routes>
+			{isLogin ? (
+				<Route path="/" element={<Home />} />
+			) : (
+				<>
+					<Route path="/" element={<LandingPage />} />
+				</>
+			)}
+			<Route path="/sign-up" element={<SignUp />} />
+			<Route path="/sign-in" element={<SignIn />} />
+			<Route path="/modul">
+				<Route path="" element={<Modul />} />
+				<Route path="detail-modul/:id" element={<DetailModul />} />
+			</Route>
+			<Route path="/quiz">
+				{/* <Route path="" element={<Quiz />} /> */}
+				<Route path="test/:id" element={<DetailQuiz />} />
+			</Route>
+		</Routes>
+	);
 };
 
 export default AppRouter;
